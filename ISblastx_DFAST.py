d0923702751b41e5071bb242155f3403fd590341
buncho <78598272+dkato2021@users.noreply.github.com>
2021-11-18 13:22:39 +0900

    Update ISblastx_DFAST.py

--- ISblastx_DFAST.py
+++ ISblastx_DFAST.py
@@ -20,9 +20,9 @@ def get_args():
     parser.add_argument('-db', '--database', default='/home_ssd/local/db/blastdb.20200904/nr', 
                        help='path to your nr database.(default:/home_ssd/local/db/blastdb.20200904/nr)')
     parser.add_argument('-t' , '--num_threads', type=int, default=16,
-                       help='num_threads in blastx.(default:16)',) 
+                       help='num threads in blastx.(default:16)',) 
     parser.add_argument('-nd', '--num_descriptions', type=int, default=50,
-                       help='num_descriptions in blastx.(default:50)')
+                       help='num descriptions in blastx.(default:50)')
     
     parser.add_argument('--Without_blast', type=bool, default=False, choices=[True, False],
                         help='If "True", blastx will not be executed.(default:False)') 
@@ -230,4 +230,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
